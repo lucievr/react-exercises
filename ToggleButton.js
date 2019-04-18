ff47d2556e44e6c1413cd7f62681a16aa7f8c99a
Lucie <46979603+lucievr@users.noreply.github.com>
2019-04-18 22:05:36 +0200

    Lists and toggle button

---
+++ ToggleButton.js
@@ -0,0 +1,51 @@
+class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
+    }
+  
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Toggle />,
+    document.getElementById('root')
+  );
+
+  class LoggingButton extends React.Component {
+    handleClick() {
+      console.log('this is:', this);
+    }
+  
+    render() {
+      // This syntax ensures `this` is bound within handleClick
+      return (
+        <button onClick={(e) => this.handleClick(e)}>
+          Click me
+        </button>
+      );
+    }
+  }
+
+  //passing arguments to event handlers
+
+<button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button>
+
+//or
+
+<button onClick={this.deleteRow.bind(this, id)}>Delete Row</button>
