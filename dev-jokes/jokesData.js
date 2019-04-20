4dc8c58d97f5550cffbae303dd9b8fa2b1562007
Lucie <46979603+lucievr@users.noreply.github.com>
2019-04-21 01:03:50 +0200

    Update dev jokes

---
+++ dev-jokes/jokesData.js
@@ -0,0 +1,44 @@
+const jokesData = [
+  {
+    id: 1,
+    punchLine:
+      "A programmer was arrested for writing unreadable code. They refused to comment."
+  },
+  {
+    id: 2,
+    question: "What did the computer do at lunchtime?",
+    punchLine: "Had a byte!"
+  },
+  {
+    id: 3,
+    question: "What did the server say to his client who was having a bad day?",
+    punchLine: "Everything's going to be 200"
+  },
+  {
+    id: 4,
+    question: "Why don't bachelors like Git?",
+    punchLine: "Because they are afraid to commit."
+  },
+  {
+    id: 5,
+    question: "What did JavaScript call his son?",
+    punchLine: "JSON!"
+  },
+  {
+    id: 6,
+    question: "A SQL developer walked into a NoSQL bar.",
+    punchLine: "They left because they couldn't find a table."
+  },
+  {
+    id: 7,
+    question: "Where did the API go to eat?",
+    punchLine: "To the RESTaurant"
+  },
+  {
+    id: 8,
+    question: "What did the psychic say to the developers?",
+    punchLine: "I see dev people."
+  }
+];
+
+export default jokesData;
